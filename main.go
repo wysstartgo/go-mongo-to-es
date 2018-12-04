1667ea9c63ec00d2139f19be9b08fdece23a89dc
wuys <yongsheng.wu@rtdream.cn>
2018-12-04 11:42:20 +0800

    修改了chan里的数据格式

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"conf"
 	"flag"
 	"fmt"
 	"github.com/siddontang/go-log/log"
@@ -11,10 +10,11 @@ import (
 	"gopkg.in/mgo.v2"
 	"io/ioutil"
 	"sync"
+	"time"
 )
 
-var host = flag.String("host", "192.168.1.38", "Elasticsearch host")
-var port = flag.Int("port", 9200, "Elasticsearch port")
+var host = flag.String("host", "192.168.1.333", "Elasticsearch host")
+var port = flag.Int("port", 9300, "Elasticsearch port")
 var c *elastic.Client
 //批处理的数量
 const BatchSize  = 500
@@ -23,9 +23,12 @@ var (
 	sourceCollection *mgo.Collection
 )
 //创建缓冲区
-var ch = make(chan [] interface{},1000)
+var ch = make(chan [] bson.M,1000)
 
 const WorkerCount  = 5        //worker数量
+
+var index = ""
+var docType = ""
 
 
 func main(){
@@ -36,9 +39,9 @@ func main(){
 	cfg.Password = ""
 	c = elastic.NewClient(cfg)
 
-	index := "rt_group"
-	docType :=  "blog"
-	mapingMap,err := ioutil.ReadFile("F:\\goworkspace\\go-zm-dev\\src\\cc\\group.mapping")
+	index = "rt_group"
+	docType =  "group"
+	mapingMap,err := ioutil.ReadFile("F:\\goworkspace\\go-mongo-to-es\\group.mapping")
 	if err != nil{
 		fmt.Println(err)
 	}
@@ -48,7 +51,7 @@ func main(){
 	}
 
 	//从mongodb取数据来建索引
-	_, sourceCollection = mongoClient.InitDB(conf.GConf.SourceMongoUrl, conf.GConf.SourceCollection)
+	_, sourceCollection = mongoClient.InitDB("mongodb://zx:zx123456@192.168.1.133:20001,192.168.1.134:20001,192.168.1.135:20001/zx", "business_group_base_info")
 	var controlWaitGroup sync.WaitGroup
 
 	for i := 1 ; i <= WorkerCount; i++{
@@ -80,14 +83,14 @@ func main(){
 	waitGroup.Add(1)
 	log.Println("start from first position!")
 	//time.Sleep(time.Second * 2)
-	go mongoClient.StartFromFirstPosition(half, &waitGroup)
+	go mongoClient.StartFromFirstPosition(half, &waitGroup,sourceCollection,BatchSize,ch)
 	log.Println("start from end position!")
 	waitGroup.Add(1)
 	if count % 2 == 0 {
-		go mongoClient.StartFromEndPosition(half, &waitGroup)
+		go mongoClient.StartFromEndPosition(half, &waitGroup,sourceCollection,BatchSize,ch)
 	} else {
 		//从后向前多查询一个
-		go mongoClient.StartFromEndPosition(half+1, &waitGroup)
+		go mongoClient.StartFromEndPosition(half+1, &waitGroup,sourceCollection,BatchSize,ch)
 	}
 	waitGroup.Wait()
 	fmt.Println("**************************")
@@ -97,22 +100,69 @@ func main(){
 /**
   注意：在这里需要传指针，不能传变量
  */
-func work(ch chan []interface{},workWaitGroup *sync.WaitGroup){
-	//var isStop = false
-	//for{
-	//	if isStop{
-	//		break
-	//	}
-	//	//接收任务
-	//	select {
-	//	case task := <- ch:
-	//		//writeTargetData(task)
-	//	case <-time.After(time.Second * 5):
-	//		fmt.Println("数据已经处理完毕，关闭协程!")
-	//		workWaitGroup.Done()
-	//		isStop = true
-	//		break
-	//	}
-	//
-	//}
+func work(ch chan []bson.M,workWaitGroup *sync.WaitGroup){
+	var isStop = false
+	for{
+		if isStop{
+			break
+		}
+		//接收任务
+		select {
+		case task := <- ch:
+			buildGroupIndex(task)
+		case <- time.After(time.Second * 5):
+			fmt.Println("数据已经处理完毕，关闭协程!")
+			workWaitGroup.Done()
+			isStop = true
+			break
+		}
+	}
 }
+
+
+
+/**
+	创建圈子索引
+ */
+func buildGroupIndex(data []bson.M){
+	length := len(data)
+	items := make([]*elastic.BulkRequest,length)
+	for i := 0; i < length; i++ {
+		bulkRequest := new(elastic.BulkRequest)
+		bulkRequest.Action = elastic.ActionCreate
+		rowData := data[i]
+		log.Println("==============",rowData)
+		//获取id
+		groupId := rowData["groupId"].(string)
+		bulkRequest.ID = groupId
+		//bulkRequest.Index = index
+		//bulkRequest.Type = docType
+		groupData := make(map[string]interface{})
+		groupData["id"] = groupId
+		groupData["title"] = rowData["title"]
+		groupData["brief"] = rowData["brief"]
+		groupData["tags_ids"] = rowData["tagIds"]
+		groupData["tags_names"] = rowData["tagNames"]
+		groupData["is_free"] = rowData["isFree"]
+		groupData["price"] = rowData["price"]
+		groupData["author"] = rowData["author"]
+		groupData["publish_time"] = rowData["publishTime"]
+		groupData["region"] = rowData["groupRegion"]
+		groupData["category_id"] = rowData["categoryId"]
+		groupData["category_name"] = rowData["categoryName"]
+		groupData["total_members"] = rowData["totalMembers"]
+		groupData["attr1"] = rowData["attr1"]
+		groupData["attr2"] = rowData["attr2"]
+		groupData["score"] = rowData["score"]
+		bulkRequest.Data = groupData
+		items[i] = bulkRequest
+	}
+	//保存
+	resp,err := c.IndexTypeBulk(index,docType,items)
+	if err != nil {
+		log.Error(err,resp.Code,":",resp.Errors)
+	}else{
+		log.Println(resp.Code,resp.Items)
+	}
+}
+
